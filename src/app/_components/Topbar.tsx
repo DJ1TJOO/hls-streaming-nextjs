b5f93b50785907c81bf404c75dbf8e3effbdf446
Thomas B <t.ferb1@gmail.com>
2023-11-01 20:38:23 +0100

    feat: added notification popup

--- src/app/_components/Topbar.tsx
+++ src/app/_components/Topbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { BellIcon, ChevronLeftIcon } from "@heroicons/react/24/outline";
 
+import NotificationMenu from "./notifications/NotificationMenu";
 import SearchBar from "./search/SearchBar";
 
 export default function Topbar() {
@@ -11,9 +12,15 @@ export default function Topbar() {
                 <ChevronLeftIcon className="h-6 w-6 text-text" />
                 <SearchBar />
             </div>
-            <div className="rounded-2xl bg-secondairy p-2.5 text-text">
-                <BellIcon className="h-5 w-5" />
-            </div>
+            <NotificationMenu
+                button={
+                    <div className="rounded-2xl bg-secondairy p-2.5 text-text">
+                        <BellIcon className="h-5 w-5" />
+                    </div>
+                }
+            >
+                asasfaf
+            </NotificationMenu>
         </header>
     );
 }
