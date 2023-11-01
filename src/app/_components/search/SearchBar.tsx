e8a1461656010ee5271ffd30becac17ac67b3ada
Thomas B <t.ferb1@gmail.com>
2023-11-01 20:23:43 +0100

    fix: changed search bar container to label

--- src/app/_components/search/SearchBar.tsx
+++ src/app/_components/search/SearchBar.tsx
@@ -4,12 +4,12 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 export default function SearchBar() {
     return (
-        <div className="flex items-center gap-3 rounded-2xl border border-text-dark px-4 py-2.5 text-text-dark">
+        <label className="flex cursor-text items-center gap-3 rounded-2xl border border-text-dark px-4 py-2.5 text-text-dark">
             <MagnifyingGlassIcon className="h-5 w-5" />
             <input
                 className="w-80 bg-transparent text-sm outline-none"
                 placeholder="Search for movies, series and more"
             />
-        </div>
+        </label>
     );
 }
