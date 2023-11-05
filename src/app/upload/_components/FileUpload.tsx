4e0ac85c73ad1b5549651c1e32e8badd3b9bb65d
Thomas B <t.ferb1@gmail.com>
2023-11-05 16:35:48 +0100

    fix: allow mkv to load and limit preview to 2 seconds to generate

--- src/app/upload/_components/FileUpload.tsx
+++ src/app/upload/_components/FileUpload.tsx
@@ -17,17 +17,21 @@ import VideoUploadRejections from "./video_upload/VideoUploadRejections";
 async function getDuration(file: File) {
     const url = URL.createObjectURL(file);
 
-    return new Promise<number>((resolve) => {
-        const audio = document.createElement("audio");
-        audio.muted = true;
-        const source = document.createElement("source");
-        source.src = url;
-        audio.preload = "metadata";
-        audio.appendChild(source);
-        audio.onloadedmetadata = function () {
-            resolve(audio.duration);
-        };
-    });
+    return Promise.race([
+        new Promise<number>((resolve) => setTimeout(() => resolve(0), 2000)),
+        new Promise<number>((resolve) => {
+            const audio = document.createElement("audio");
+            audio.muted = true;
+            const source = document.createElement("source");
+            source.src = url;
+
+            audio.preload = "metadata";
+            audio.appendChild(source);
+            audio.onloadedmetadata = function () {
+                resolve(audio.duration);
+            };
+        }),
+    ]);
 }
 
 export default function FileUpload() {
@@ -40,29 +44,32 @@ export default function FileUpload() {
             fileRejections: FileRejection[],
             event: DropEvent
         ) => void
-    >(async (acceptedFiles, rejectedFiles) => {
-        if (acceptedFiles?.length) {
-            const acceptedFilesMapped = await Promise.all(
-                acceptedFiles.map(async (file) =>
-                    Object.assign(file, {
-                        preview: URL.createObjectURL(file),
-                        duration: await getDuration(file),
-                        upload: 0,
-                        uploadingResponse: null,
-                    })
-                )
-            );
+    >(
+        async (acceptedFiles, rejectedFiles) => {
+            if (acceptedFiles?.length) {
+                const acceptedFilesMapped = await Promise.all(
+                    acceptedFiles.map(async (file) =>
+                        Object.assign(file, {
+                            preview: URL.createObjectURL(file),
+                            duration: await getDuration(file),
+                            upload: 0,
+                            uploadingResponse: null,
+                        })
+                    )
+                );
 
-            setFiles((previousFiles) => [
-                ...previousFiles,
-                ...acceptedFilesMapped,
-            ]);
-        }
+                setFiles((previousFiles) => [
+                    ...previousFiles,
+                    ...acceptedFilesMapped,
+                ]);
+            }
 
-        if (rejectedFiles?.length) {
-            setAnimationPlayed(false);
-        }
-    }, []);
+            if (rejectedFiles?.length) {
+                setAnimationPlayed(false);
+            }
+        },
+        [setFiles]
+    );
 
     const validator = useCallback<
         <T extends File>(file: T) => FileError | FileError[] | null
@@ -81,7 +88,7 @@ export default function FileUpload() {
     const { getRootProps, getInputProps, isDragActive, fileRejections } =
         useDropzone({
             accept: {
-                "video/*": [],
+                "video/*": [".mkv"],
             },
             maxSize: 5368709120, //5gb
             onDrop,
