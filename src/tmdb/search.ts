5180f6fd1647682c2c064db61efa09a0ceb954b9
Thomas B <t.ferb1@gmail.com>
2023-11-05 16:36:56 +0100

    feat: changed and optimized search

--- src/tmdb/search.ts
+++ src/tmdb/search.ts
@@ -2,12 +2,14 @@ import {
     Episode,
     MovieDb,
     MovieResult,
-    PersonResult,
     SearchMultiResponse,
+    ShowResponse,
     TvResult,
+    TvSeasonResponse,
 } from "moviedb-promise";
 
 import {
+    SeasonEpisode,
     extractName,
     extractSeasonEpisode,
     extractYear,
@@ -16,27 +18,19 @@ import {
 
 const db = new MovieDb(process.env.NEXT_PUBLIC_TMDB_API_KEY!);
 
-type SearchResult =
+export type SearchResult =
     | {
-          result: MovieResult;
+          movieSeries: MovieResult;
           episode: null;
-          seasonEpisode: null;
       }
     | {
-          result: TvResult;
-          episode: (Episode & { runtime?: number }) | null;
-          seasonEpisode: {
-              season: number | null;
-              episode: number | null;
-          } | null;
+          movieSeries: TvResult;
+          episode: Episode & { runtime?: number };
       };
 
 export async function search(fileName: string) {
     const preparedFileName = prepareFileName(fileName);
     const name = extractName(preparedFileName);
-    const seasonEpisode = extractSeasonEpisode(preparedFileName);
-    const year = extractYear(preparedFileName);
-    console.log(year, preparedFileName);
 
     const searchResult = await db
         .searchMulti({
@@ -49,108 +43,208 @@ export async function search(fileName: string) {
                 }) as SearchMultiResponse
         );
 
-    const results =
-        (searchResult.results?.filter((x) => x.media_type !== "person") as (
-            | MovieResult
-            | TvResult
-        )[]) ?? [];
-    if (results.length < 1) return null;
+    return await resolveSearch(searchResult, preparedFileName);
+}
 
-    const mappedResults = await Promise.all(
-        results.map(async (x) => {
-            if (x.media_type === "movie") {
-                return {
-                    result: x,
-                    episode: null,
-                    seasonEpisode: null,
-                };
-            }
+async function resolveSearch(
+    searchResult: SearchMultiResponse,
+    preparedFileName: string
+) {
+    if (!searchResult.results || searchResult.results.length < 1) return null;
 
-            const foundSeasonEpisode = await findSeasonEpisode(
-                seasonEpisode,
-                x
-            );
+    // Remove persons as we don't care about those
+    const resultsWithoutPersons = searchResult.results.filter(
+        (x) => x.media_type !== "person"
+    ) as (MovieResult | TvResult)[];
+    if (resultsWithoutPersons.length < 1) return null;
 
-            let episode = null;
-            if (
-                foundSeasonEpisode?.season &&
-                foundSeasonEpisode.episode &&
-                x.id
-            ) {
-                episode = await db.episodeInfo({
-                    id: x.id,
-                    season_number: foundSeasonEpisode.season,
-                    episode_number: foundSeasonEpisode.episode,
-                });
-            }
-
-            return {
-                result: x,
-                episode,
-                seasonEpisode: foundSeasonEpisode,
-            };
-        })
+    // Find episodes
+    const resultsWithEpisodes = await resolveEpisodes(
+        resultsWithoutPersons,
+        preparedFileName
     );
-    const best = findBestResult(mappedResults, year);
+    const best = findBestResult(resultsWithEpisodes, preparedFileName);
 
     return {
         best,
-        info: { name, seasonEpisode, year },
-        results: mappedResults,
+        results: resultsWithEpisodes,
+    } as {
+        best: SearchResult;
+        results: SearchResult[];
     };
 }
 
-function findBestResult(results: SearchResult[], year: string | null) {
-    let bestResult = results[0];
+async function resolveEpisodes(
+    resultsWithoutPersons: (MovieResult | TvResult)[],
+    preparedFileName: string
+) {
+    const seasonEpisode = extractSeasonEpisode(preparedFileName);
+    const episodesPromises = resultsWithoutPersons.map(
+        resolveEpisodesMap.bind(undefined, seasonEpisode)
+    );
 
-    if (year) {
-        const findBest = results.find(
-            (x) =>
-                (x.result.media_type === "tv" &&
-                    x.result.first_air_date?.includes(year)) ||
-                (x.result.media_type === "movie" &&
-                    x.result.release_date?.includes(year))
-        );
-        if (findBest) bestResult = findBest;
-    }
-
-    return bestResult;
+    const resultsWithEpisodes = await Promise.all(episodesPromises);
+    return resultsWithEpisodes.flat();
 }
 
-async function findSeasonEpisode(
-    seasonEpisode: {
-        season: number | null;
-        episode: number | null;
-    } | null,
-    result: TvResult | MovieResult
+async function resolveEpisodesMap(
+    seasonEpisode: SeasonEpisode,
+    result: MovieResult | TvResult
 ) {
-    if (result.media_type !== "tv") {
+    if (result.media_type === "movie") {
+        return {
+            movieSeries: result,
+            episode: null,
+        };
+    }
+
+    // No valid id so cannot search for episodes
+    if (!result.id) {
+        return [];
+    }
+
+    // Get seaons and info
+    // Prefetch
+    const preTv = await db.tvInfo({
+        id: result.id,
+    });
+    // With seasons
+    const tv = (await db.tvInfo({
+        id: result.id,
+        append_to_response: preTv.seasons
+            ?.map((x) => `season/${x.season_number}`)
+            .join(","),
+    })) as ShowResponse & { [key: `season/${number}`]: TvSeasonResponse };
+
+    if (!tv.seasons) return [];
+    const tvSeasons = tv.seasons.flatMap((season) =>
+        typeof season.season_number !== "undefined"
+            ? tv[`season/${season.season_number}`]
+            : []
+    );
+
+    // No season or episode specified, return first episode or get specific episode
+    if (
+        !seasonEpisode ||
+        (seasonEpisode.season === null && seasonEpisode.episode === null) ||
+        (seasonEpisode.season !== null && seasonEpisode.episode !== null)
+    ) {
+        let seasonNumber = seasonEpisode?.season ?? 1;
+        let episodeNumber = seasonEpisode?.episode ?? 1;
+
+        // Return episode
+        const episode = resolveEpisode(tvSeasons, seasonNumber, episodeNumber);
+        if (!episode) return [];
+
+        return {
+            movieSeries: result,
+            episode,
+        };
+    }
+
+    // No episode speficified, return first of season
+    if (seasonEpisode.season !== null && seasonEpisode.episode === null) {
+        const episode = resolveEpisode(tvSeasons, seasonEpisode.season, 1);
+        if (!episode) return [];
+
+        return {
+            movieSeries: result,
+            episode,
+        };
+    }
+
+    // No season speficied, return for all seasons
+    const episodeNumber = seasonEpisode.episode;
+    if (episodeNumber === null) {
+        return [];
+    }
+
+    // Find all best episodes for each season
+    const resolvedEpisodes = tvSeasons.map((season) =>
+        season.season_number
+            ? resolveEpisode(tvSeasons, season.season_number, episodeNumber)
+            : null
+    );
+    // Remove null and duplicates
+    const resolvedEpisodesFiltered = resolvedEpisodes.filter(
+        (x, i) => x !== null && resolvedEpisodes.indexOf(x) === i
+    ) as Episode[];
+
+    return resolvedEpisodesFiltered.map((episode) => ({
+        movieSeries: result,
+        episode: episode,
+    }));
+}
+
+function resolveEpisode(
+    tvSeasons: TvSeasonResponse[],
+    seasonNumber: number,
+    episodeNumber: number
+) {
+    let season: TvSeasonResponse | null = null;
+
+    // If season doesn't exist replace with closest
+    if (!tvSeasons.some((x) => x.season_number === seasonNumber)) {
+        season = tvSeasons.reduce((prev, curr) =>
+            // Find closest to intended season
+            Math.abs((curr.season_number ?? -1) - seasonNumber) <
+            Math.abs((prev.season_number ?? -1) - seasonNumber)
+                ? curr
+                : prev
+        );
+    } else {
+        season =
+            tvSeasons.find((x) => x.season_number === seasonNumber) ?? null;
+    }
+
+    // No season found
+    if (!season) {
         return null;
     }
 
-    let bestSeasonEpisode: {
-        season: number | null;
-        episode: number | null;
-    } = { season: null, episode: null };
+    let episode: Episode | null = null;
 
-    if (!result.id) return bestSeasonEpisode;
-
-    const tvResult = await db.tvInfo(result.id);
-
-    if (
-        seasonEpisode?.season &&
-        tvResult.number_of_seasons &&
-        seasonEpisode.season <= tvResult.number_of_seasons
-    ) {
-        bestSeasonEpisode.season = seasonEpisode.season;
-    }
-    if (
-        seasonEpisode?.episode &&
-        tvResult.number_of_episodes &&
-        seasonEpisode.episode <= tvResult.number_of_episodes
-    ) {
-        bestSeasonEpisode.episode = seasonEpisode.episode;
+    // If season doesn't exist replace with closest
+    if (!season.episodes?.some((x) => x.episode_number === episodeNumber)) {
+        episode =
+            season.episodes?.reduce((prev, curr) =>
+                // Find closest to intended season
+                Math.abs((curr.episode_number ?? -1) - episodeNumber) <
+                Math.abs((prev.episode_number ?? -1) - episodeNumber)
+                    ? curr
+                    : prev
+            ) ?? null;
+    } else {
+        episode =
+            season.episodes.find((x) => x.episode_number === episodeNumber) ??
+            null;
     }
 
-    return bestSeasonEpisode;
+    // No episode found
+    if (!episode) {
+        return null;
+    }
+
+    // Attributes missing
+    if (
+        !episode.id ||
+        typeof episode.season_number === "undefined" ||
+        typeof episode.episode_number === "undefined"
+    ) {
+        return null;
+    }
+
+    return episode;
+}
+
+function findBestResult(results: SearchResult[], preparedFileName: string) {
+    const year = extractYear(preparedFileName);
+    if (!year) return results[0];
+
+    const bestResult = results.find((x) =>
+        x.movieSeries.media_type === "tv"
+            ? x.movieSeries.first_air_date?.includes(year)
+            : x.movieSeries.release_date?.includes(year)
+    );
+    return bestResult ?? results[0];
 }
