b9dce64617a8b4c3bc45e8a4a9467c34a0032d0a
Thomas B <t.ferb1@gmail.com>
2023-11-05 16:38:01 +0100

    fix: search type update on description

--- src/app/upload/_components/video_upload/VideoDescription.tsx
+++ src/app/upload/_components/video_upload/VideoDescription.tsx
@@ -10,10 +10,10 @@ export default function VideoDescription() {
     if (!currentResult) return <div className="flex gap-3"></div>;
 
     const title =
-        currentResult.result.media_type === "movie"
-            ? currentResult.result.title
-            : currentResult.result.name;
-    const description = currentResult.result.overview;
+        currentResult.movieSeries.media_type === "movie"
+            ? currentResult.movieSeries.title
+            : currentResult.movieSeries.name;
+    const description = currentResult.movieSeries.overview;
 
     return (
         <div className="flex gap-3">
@@ -23,32 +23,32 @@ export default function VideoDescription() {
                     <span className="text-text-dark">
                         (
                         {formatYear(
-                            (currentResult.result.media_type === "movie"
-                                ? currentResult.result.release_date
-                                : currentResult.result.first_air_date) ?? ""
+                            (currentResult.movieSeries.media_type === "movie"
+                                ? currentResult.movieSeries.release_date
+                                : currentResult.movieSeries.first_air_date) ??
+                                ""
                         )}
                         )
                     </span>
                 </h1>
                 <p className="text-sm text-text-dark">{description}</p>
             </div>
-            {currentResult.episode &&
-                currentResult.result.media_type === "tv" && (
-                    <div className="flex w-full flex-col gap-1">
-                        <h1 className="text-xl font-medium text-text">
-                            {currentResult.episode.name}{" "}
-                            <span className="text-text-dark">
-                                •{" "}
-                                {formatTime(
-                                    (currentResult.episode.runtime || 0) * 60
-                                )}
-                            </span>
-                        </h1>
-                        <p className="text-sm text-text-dark">
-                            {currentResult.episode.overview}
-                        </p>
-                    </div>
-                )}
+            {currentResult.episode && (
+                <div className="flex w-full flex-col gap-1">
+                    <h1 className="text-xl font-medium text-text">
+                        {currentResult.episode.name}{" "}
+                        <span className="text-text-dark">
+                            •{" "}
+                            {formatTime(
+                                (currentResult.episode.runtime || 0) * 60
+                            )}
+                        </span>
+                    </h1>
+                    <p className="text-sm text-text-dark">
+                        {currentResult.episode.overview}
+                    </p>
+                </div>
+            )}
         </div>
     );
 }
